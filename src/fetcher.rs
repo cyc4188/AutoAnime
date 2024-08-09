c2e1582d6108281e65632c0f1f3fb3cc260f5a10
cyc4188 <cyche5ter@163.com>
2024-08-09 11:37:36 +0800

    feat: timed fetch

--- src/fetcher.rs
+++ src/fetcher.rs
@@ -23,7 +23,6 @@ impl Fetcher {
 
     /// fetch feeds
     pub async fn fetch(&mut self) -> Result<()> {
-        // TODO add filter
         for feed in self.config.feeds() {
             let content = self
                 .request_client
@@ -49,15 +48,22 @@ impl Fetcher {
             // filter items
             channel.items.retain(|item| {
                 if let Some(pub_date) = item.pub_date() {
-                    let mut pub_date = pub_date.to_string();
-                    pub_date.push_str("+08:00");
-                    let pub_date = DateTime::parse_from_rfc3339(pub_date.as_str())
-                        .unwrap()
-                        .with_timezone(&Local {});
-                    return pub_date > self.last_update_time;
+                    let date_time = pub_date2date_time(pub_date);
+                    return date_time > self.last_update_time;
+                }
+                // Note: mikan pub date is in torrent
+                if let Some(torrent) = &item.torrent {
+                    if let Some(pub_date) = torrent.pub_date.as_ref() {
+                        let date_time = pub_date2date_time(pub_date);
+                        return date_time > self.last_update_time;
+                    }
                 }
                 true
             });
+
+            if channel.items().is_empty() {
+                continue;
+            }
 
             for subscriber in &feed.subscriber {
                 subscriber.notify(&self.config, &channel).await?;
@@ -66,4 +72,12 @@ impl Fetcher {
         self.last_update_time = Local::now();
         Ok(())
     }
+}
+
+fn pub_date2date_time(pub_date: &str) -> DateTime<Local> {
+    let mut pub_date = pub_date.to_string();
+    pub_date.push_str("+08:00");
+    DateTime::parse_from_rfc3339(pub_date.as_str())
+        .unwrap()
+        .with_timezone(&Local {})
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
-use config::get_config;
+use std::sync::Arc;
+
+use config::{get_config, Config};
+use delay_timer::{
+    entity::DelayTimerBuilder,
+    error::TaskError,
+    prelude::{Task, TaskBuilder},
+};
+use tokio::sync::Mutex;
 use tracing::{info, Level};
 use tracing_subscriber::FmtSubscriber;
 
@@ -13,13 +21,40 @@ async fn main() -> anyhow::Result<()> {
 
     let path = "./config.yaml";
     let config = get_config(path)?;
-    info!("get config");
+    info!("[Config] get config");
 
-    let mut fetcher = fetcher::Fetcher::new(config);
-    fetcher.fetch().await?;
-    info!("fetch done");
+    let delay_timer = DelayTimerBuilder::default()
+        .tokio_runtime_by_default()
+        .build();
+
+    delay_timer.add_task(fetch_task(config)?)?;
+    info!("[Task] add task");
+
+    loop {
+        tokio::time::sleep(std::time::Duration::from_secs(30)).await;
+    }
 
     Ok(())
+}
+
+fn fetch_task(config: Config) -> anyhow::Result<Task, TaskError> {
+    let mut task_builder = TaskBuilder::default();
+    let fetcher = Arc::new(Mutex::new(fetcher::Fetcher::new(config)));
+
+    let body = move || {
+        let fetcher = fetcher.clone();
+        async move {
+            if let Err(e) = fetcher.lock().await.fetch().await {
+                info!("[Task] Fetch Error: {}", e);
+            }
+            info!("[Task] Fetch Done");
+        }
+    };
+
+    task_builder
+        .set_task_id(1)
+        .set_frequency_repeated_by_hours(6)
+        .spawn_async_routine(body)
 }
 
 fn log_init() {
